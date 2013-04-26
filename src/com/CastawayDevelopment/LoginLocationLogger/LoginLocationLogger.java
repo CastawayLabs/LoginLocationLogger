1cf120605a690205d6c8435c882aa923b8a18926
Alan Campbell <echobob@theminequest.com>
2013-04-26 00:52:54 -0400

    Basic logger stuffs

--- src/com/CastawayDevelopment/LoginLocationLogger/LoginLocationLogger.java
+++ src/com/CastawayDevelopment/LoginLocationLogger/LoginLocationLogger.java
@@ -19,33 +19,22 @@ package com.CastawayDevelopment.LoginLocationLogger;
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.configuration.file.FileConfiguration;
 
 public class LoginLocationLogger extends JavaPlugin {
 	 
-	/**
-	 * 
-	 * 
-	 */
-	public void onEnable() {
-		
-	}
+	private File loginLogFile;
 	
-	/**
-	 * 
-	 *  
-	 *  @param number
-	 */
-	public void newLoggerFile(int number){
-		
-		
-	}
-	 
 	/**
 	 * 
 	 * @param event
@@ -54,7 +43,85 @@ public class LoginLocationLogger extends JavaPlugin {
     public void playerJoin(PlayerJoinEvent event) {
 		//On player join send them the message from config.yml
 		Player player = event.getPlayer();
-    	player.getLocation();
+    	Location location = player.getLocation();    	
+    	logToFile(player, location);
 	}
 	
+	public void onEnable() {
+		
+		File dataFolder = getDataFolder();
+        if(!dataFolder.exists())
+    	{
+            dataFolder.mkdir();
+        }
+        this.getLogger().log(Level.INFO, "It's Working, I think. Echo");
+		newLoggerFile("LocationLog_", 1);
+	}
+	
+	/**
+	 * Makes new logger file, if file already exists, make a new one.
+	 * @param filename
+	 * @param number
+	 * @return
+	 */
+	public File newLoggerFile(String filename, int number){
+		loginLogFile = new File(getDataFolder(), (filename+number+".txt"));
+        if (!loginLogFile.exists())
+        {
+            try 
+            {
+            	loginLogFile.createNewFile();
+            }
+	        catch (IOException e) 
+	        {
+	        	e.printStackTrace();
+			}
+        }
+        else if (loginLogFile.exists())
+        {
+        	while(loginLogFile.exists())
+        	{
+        		number++;
+        		loginLogFile = new File(getDataFolder(), (filename+number+".txt"));
+        	}
+        	try 
+            {
+        		loginLogFile.createNewFile();
+            }
+	        catch (IOException e) 
+	        {
+	        	e.printStackTrace();
+			}
+        }
+        return loginLogFile;
+	}
+
+	/**
+	 * Logs a entry to the file.
+	 * @param file
+	 * @param player
+	 * @param location
+	 */
+	public void logToFile(Player player, Location location){
+		
+		String worldName = location.getWorld().getName().toString();
+		String playerName = player.getName().toString();
+		int x = location.getBlockX();
+		int y = location.getBlockY();
+		int z = location.getBlockZ();
+		
+		String loginLocEntry = (playerName + "   " + worldName + "   " + x + "   " + y + "   " + z); 
+		try
+        {
+			FileWriter fw = new FileWriter(loginLogFile, true);
+	        PrintWriter pw = new PrintWriter(fw);
+			pw.println(loginLocEntry); 
+	        pw.flush();
+	        pw.close();
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
